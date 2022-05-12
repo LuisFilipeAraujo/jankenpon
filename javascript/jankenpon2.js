cc64138cfaf46aee78619c9b81069de86782820c
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2022-05-12 02:44:47 -0300

    Buttons Spock and Lizard added. Video added and main image modified

--- javascript/jankenpon2.js
+++ javascript/jankenpon2.js
@@ -4,7 +4,7 @@ const resultDisplay = document.createElement('h2')
 const gameGrid = document.getElementById('game')
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
-const choices = ['rock', 'paper', 'scissors']
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 let userChoice
 let computerChoice
 
@@ -32,19 +32,58 @@ for (let i = 0; i < choices.length; i++) {
 const getResult = () => {
   switch (userChoice + computerChoice) {
     case 'scissorspaper':
+    case 'scissorslizard':
     case 'rockscissors':
+    case 'rocklizard':
     case 'paperrock':
+    case 'paperspock':
+    case 'lizardspock':
+    case 'lizardpaper':
+    case 'spockscissors':
+    case 'spockrock':
       resultDisplay.innerHTML = "YOU WIN!"
       break
+
     case 'paperscissors':
+    case 'paperlizard':
     case 'scissorsrock':
+    case 'scissorsspock':
     case 'rockpaper':
+    case 'rockspock':
+    case 'lizardscissors':
+    case 'lizardrock':
+    case 'spocklizard':
+    case 'spockpaper':
       resultDisplay.innerHTML = "YOU LOSE!"
       break
+
     case 'paperpaper':
     case 'scissorsscissors':
     case 'rockrock':
+    case 'lizardlizard':
+    case 'spockspock':
     resultDisplay.innerHTML = "ITS A DRAW!"
     break
   }
 }
+
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
+
+function makeBig() { 
+    myVideo.width = 560; 
+} 
+
+function makeSmall() { 
+    myVideo.width = 320; 
+} 
+
+function makeNormal() { 
+    myVideo.width = 420; 
+} 
